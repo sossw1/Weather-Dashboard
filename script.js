2b711b573c99acd5ed9d87ffba154ee17139941f
sossw1 <sossw1@gmail.com>
2020-12-05 18:13:39 -0500

    API Call

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ var currentCity = "";
 var apiKey = "238865a2d2158723334cf88f7fd88c92";
 
 function requestWeather(){
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + currentCity + "&appid=" + apiKey;
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    });
 }
 
 // Event Listener for city form
